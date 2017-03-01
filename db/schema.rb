dae7f368681f5c7269e19050f079cdeb097564e1
Atsuko <oda-atsuko@av.avex.co.jp>
2017-03-01 05:57:38 +0000

    課題(途中)店舗紹介カラムをrestaurantモデルに追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170301050546) do
+ActiveRecord::Schema.define(version: 20170301055002) do
 
   create_table "genres", force: :cascade do |t|
     t.string   "name"
@@ -48,6 +48,11 @@ ActiveRecord::Schema.define(version: 20170301050546) do
     t.float    "longitude"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+<<<<<<<<< saved version
+    t.text     "introduction"
+=========
+    
+>>>>>>>>> local version
   end
 
   create_table "reviews", force: :cascade do |t|
