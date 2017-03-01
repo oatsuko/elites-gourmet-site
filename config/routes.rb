57dd43b16a292b4233229798f40ecdf6950d8f2b
Atsuko <oda-atsuko@av.avex.co.jp>
2017-03-01 04:13:25 +0000

    02_実習_ソーシャルログイン(補足ソーシャルログイン設定 facebookとLINEはまだ...

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'restaurants/index'
+  get 'restaurants/index', as: 'user_root'
   root 'restaurants#index'
-  devise_for :users
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks'
+  }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
